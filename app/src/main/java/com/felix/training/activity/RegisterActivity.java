f4264ff9b474ca3d253f84d733853d81e667151e
Efje6 <adikefuad10@gmail.com>
2020-05-28 11:42:11 +0700

    revisi

---
+++ app/src/main/java/com/felix/training/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.felix.training.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.felix.training.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
